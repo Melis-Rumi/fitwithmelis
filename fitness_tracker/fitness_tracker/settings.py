08997f37e322bb1e2664a774196d4f245dd8709c
Melis-Rumi <119591358+Melis-Rumi@users.noreply.github.com>
2025-03-29 01:34:34 +0000

    Update settings.py

--- fitness_tracker/fitness_tracker/settings.py
+++ fitness_tracker/fitness_tracker/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',  # Runs first
     'fitness_tracker.middleware.ImpersonateUserMiddleware',  # Runs after AuthenticationMiddleware
     'django.contrib.messages.middleware.MessageMiddleware',
